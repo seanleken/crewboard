aa591f7ef04ffdb768e3b648eef84a306303497a
seanleken <seanleken43@gmail.com>
2026-03-19 01:12:02 +0300

    Remove block time params

--- lib/simbrief.ts
+++ lib/simbrief.ts
@@ -8,8 +8,6 @@ interface FlightForDispatch {
 
 export function buildSimBriefUrl(flight: FlightForDispatch, airlineIcao: string): string {
   const fltnum = flight.flightNumber.replace(/^[A-Z]+/, '')
-  const steh = Math.floor(flight.durationMinutes / 60)
-  const stem = flight.durationMinutes % 60
 
   const params = new URLSearchParams({
     airline: airlineIcao,
@@ -17,8 +15,6 @@ export function buildSimBriefUrl(flight: FlightForDispatch, airlineIcao: string)
     type: flight.aircraftIcao,
     orig: flight.originIcao,
     dest: flight.destinationIcao,
-    steh: String(steh),
-    stem: String(stem),
   })
 
   return `https://dispatch.simbrief.com/options/custom?${params.toString()}`
